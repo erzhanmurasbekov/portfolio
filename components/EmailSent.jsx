49bff970557292d86534a4e20061dd5d684513f1
erzhanmurasbekov <erzhanmurasbekov@gmail.com>
2024-04-30 16:24:03 -0700

    Initial commit

---
+++ components/EmailSent.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const EmailSent = () => {
+  return (
+    <div className="bg-green-200/30 px-4 rounded-full absolute right-16 top-14 w-32 flex justify-between animate-ping-once opacity-100 transition-opacity duration-500">
+      <p className="text-xs text-white mt-1">Email sent</p>
+      <Image
+        className={` right-0 `}
+        src={"/check.png"}
+        width={25}
+        height={50}
+      />
+    </div>
+  );
+};
+
+export default EmailSent;
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,107 @@
+import Circles from "../../components/Circles";
+import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import EmailSent from "../../components/EmailSent";
 const Contact = () => {
-  return <div>Contact</div>;
+  const [message, setMessage] = useState(
+    "Hello! I would like to offer you to work for our company!"
+  );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [showCheckmark, setShowCheckmark] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      "service_ujgp8x8",
+      "template_7czv69v",
+      e.target,
+      "DiYtUc-hEEZRGgzhB"
+    );
+    showAndHideCheckmark();
+  };
+  const showAndHideCheckmark = () => {
+    setShowCheckmark(true);
+    setTimeout(() => {
+      setShowCheckmark(false);
+    }, 1000);
+  };
+
+  return (
+    <div className="h-full bg-primary/30">
+      {showCheckmark && <EmailSent />}
+      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+        <div className="flex flex-col w-full max-w-[700px]">
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            exit="hidden"
+            animate="show"
+            className="h2 text-center mb-12">
+            Lets <span className="text-accent">connect.</span>
+          </motion.h2>
+          <motion.form
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            exit="hidden"
+            animate="show"
+            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+            onSubmit={handleSubmit}>
+            <div className="flex gap-x-6 w-full">
+              <input
+                type="text"
+                placeholder="Name"
+                className="input"
+                name="name_from"
+                required
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <input
+                type="email"
+                placeholder="email"
+                className="input"
+                name="email_from"
+                required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <input
+              type="text"
+              placeholder="subject"
+              className="input"
+              onChange={(e) => {
+                setSubject(e.target.value);
+              }}
+            />
+            <textarea
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              placeholder="message"
+              className="textarea"
+              name="message"
+              value={message}></textarea>
+            <button
+              onClick={showAndHideCheckmark}
+              className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex justify-center items-center overflow-hidden hover:border-accent group">
+              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                Lets talk
+              </span>
+              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+            </button>
+          </motion.form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
