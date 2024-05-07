bfb6e60b84de00a31cf3b5d8d8c0e626c77ff09e
erzhanmurasbekov <erzhanmurasbekov@gmail.com>
2024-05-07 00:47:03 -0700

    main page done

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ const Home = () => {
             innovation drives every pixel.`;
   return (
     <div className="bg-primary/60 h-full">
-      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10 ">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto ">
+      <div className="w-full custom-height xl:h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10 ">
+        <div className="text-center flex flex-col justify-center xl:pt-1 xl:text-left h-full container mx-auto ">
           <motion.h1
             className="h1"
             variants={fadeIn("down", 0.2)}
